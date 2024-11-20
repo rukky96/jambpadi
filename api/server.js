777482dc3c6594c5de85b8e9605fdc79d4843e16
Akproko Kelvin <akprokokelvin@gmail.com>
2024-11-21 00:02:09 +0100

    changed directory

--- api/server.js
+++ api/server.js
@@ -10,46 +10,46 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use('/api', router);
 
-app.use(express.static(path.join(__dirname, "./public")))
+app.use(express.static(path.join(__dirname, "/../public")))
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'login.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'login.html'));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'register.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'register.html'));
 })
 
 app.get('/password-reset-email', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'public', 'password_reset_email.html'))
+    res.sendFile(path.join(__dirname, '/../public', 'password_reset_email.html'))
 })
 
 app.get('/verify-email-otp', (req, res)=>{
-    res.sendFile(path.join(__dirname, "public", 'verify_email_otp.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'verify_email_otp.html'));
 })
 
 app.get('/reset-password-otp', (req, res)=>{
-    res.sendFile(path.join(__dirname, "public", 'reset_password_otp.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'reset_password_otp.html'));
 })
 
 app.get('/reset-password', (req, res)=>{
-    res.sendFile(path.join(__dirname, "public", 'password_reset.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'password_reset.html'));
 })
 
 app.get('', (req, res)=>{
-    res.sendFile(path.join(__dirname, "public", 'homepage.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'homepage.html'));
 })
 
 app.get('/user/dashboard/test', (req, res)=>{
-    res.sendFile(path.join(__dirname, "public", 'test.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'test.html'));
 })
 
 app.get('/pricing', (req, res)=>{
-    res.sendFile(path.join(__dirname, "public", 'pricing.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'pricing.html'));
 })
 
 app.get('/enterprise', (req, res)=>{
-    res.sendFile(path.join(__dirname, "public", 'enterprise.html'));
+    res.sendFile(path.join(__dirname, "/../public", 'enterprise.html'));
 })
 
 
